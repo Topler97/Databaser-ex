78f34ac95a14681475792e61bc6525da1117fb16
Topler97 <yhjs23.toplam@folkuniversitetet.nu>
2025-01-06 18:46:13 +0100

    Last update, hope it works

--- index.js
+++ index.js
@@ -1,16 +1,22 @@
-const { log } = require("console");
-const express = require("express");
-const mongoose = require(`mongoose`);
+const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
-
 app.use(express.json());
 
-const PORT = 3000;
-const URL = `127.0.0.1`
+const movieRouter = require('./movies');
+const userRouter = require('./users');
+const reviewRouter = require('./reviews');
+
+app.use('/', movieRouter);
+app.use('/', reviewRouter);
+app.use('/', userRouter);
+
+const PORT = 1337
+const URL = '127.0.0.1'
+
+mongoose.connect('')
 
 app.listen(PORT, URL, () => {
-    console.log(`connected to http://${URL}:${PORT}`);
-});
-
-
+    console.log(`listen to ${URL}:${PORT}`)
+})
